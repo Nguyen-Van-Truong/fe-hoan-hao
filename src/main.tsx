aba14a72964fe385316f57449df73ac5c3a8bb3a
gacon3640 <gacon3640@gmail.com>
2025-03-20 02:27:07 +0000

    fix deploy vercel

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,12 @@ import { BrowserRouter } from "react-router-dom";
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
 
+// Fix for useLayoutEffect warning in SSR
+if (typeof window === "undefined") {
+  // @ts-ignore
+  React.useLayoutEffect = React.useEffect;
+}
+
 const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
